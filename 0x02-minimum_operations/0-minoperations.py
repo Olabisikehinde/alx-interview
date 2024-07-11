2a59c339177ed30d5af80b19308ac7c77bf37571
Olabisikehinde <140151047+Olabisikehinde@users.noreply.github.com>
2024-07-11 17:50:04 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,92 +1,60 @@
 #!/usr/bin/python3
-'''The minimum operations coding challenge'''
+'''Minimum Operations python3 challenge'''
 
 
 def minOperations(n):
-    '''Computes the fewest number of operations needed to result
-    in exactly n H characters in this file.
-    if not isinstance(n, int):
-        return 0
+    '''calculates the fewest number of
+    operations needed to result in exactly n H
+    characters in this file.
+    Returns:
+        Integer : if n is impossible to achieve, return 0
     '''
-    ops_count = 0
-    clipboard = 0
-    done = 1
-    # print('H', end='')
-    while done < n:
+    pasted_chars = 1  # how many chars in the file
+    clipboard = 0  # how many H's copied
+    counter = 0  # operations counter
+
+    while pasted_chars < n:
+        # if did not copy anything yet
         if clipboard == 0:
-            # init (the first copy all and paste)
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-            # print('-(11)->{}'.format('H' * done), end='')
-        elif n - done > 0 and (n - done) % done == 0:
-            # copy all and paste
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-            # print('-(11)->{}'.format('H' * done), end='')
-        elif clipboard > 0:
+            # copyall
+            clipboard = pasted_chars
+            # increment operations counter
+            counter += 1
+
+        # if haven't pasted anything yet
+        if pasted_chars == 1:
             # paste
-            done += clipboard
-            ops_count += 1
-            # print('-(01)->{}'.format('H' * done), end='')
-    # print('')
-    return ops_count
+            pasted_chars += clipboard
+            # increment operations counter
+            counter += 1
+            # continue to next loop
+            continue
 
-test_0_minoperations.txt
-'''
-Run the command below to execute these test cases.
-python3 -m doctest -v test_0_minoperations.txt
+        remaining = n - pasted_chars  # remaining chars to Paste
+        # check if impossible by checking if clipboard
+        # has more than needed to reach the number desired
+        # which also means num of chars in file is equal
+        # or more than in the clipboard.
+        # in both situations it's impossible to achieve n of chars
+        if remaining < clipboard:
+            return 0
 
-Operations path format:
-Character in file: H
-operations: copy all (bit 1) or paste (bit 0),
-    e.g.; copy all and paste -> 11
-    e.g.; paste only -> 01
+        # if can't be devided
+        if remaining % pasted_chars != 0:
+            # paste current clipboard
+            pasted_chars += clipboard
+            # increment operations counter
+            counter += 1
+        else:
+            # copyall
+            clipboard = pasted_chars
+            # paste
+            pasted_chars += clipboard
+            # increment operations counter
+            counter += 2
 
->>> minOperations = __import__('0-minoperations').minOperations
->>> minOperations(0)
-0
->>> minOperations(-1)
-0
->>> minOperations(1.4)
-0
->>> minOperations('1')
-0
-
-# H
->>> minOperations(1)
-0
-
-# H-(11)->HH
->>> minOperations(2)
-2
-
-# H-(11)->HH-(11)->HHHH
-# H-(11)->HH-(01)->HHH-(01)->HHHH
->>> minOperations(4)
-4
-
-# H-(11)->HH-(01)->HHH-(01)->HHHH-(01)->HHHHH
->>> minOperations(5)
-5
-
-# H-(11)->HH-(01)->HHH-(01)->HHHH-(01)->HHHHH-(01)->HHHHHH-(01)->HHHHHHH
->>> minOperations(7)
-7
-
-# H-(11)->HH-(01)->HHH-(01)->HHHH-(01)->HHHHH-(01)->HHHHHH-(01)->HHHHHHH-(01)->HHHHHHHH-(01)->HHHHHHHHH-(01)->HHHHHHHHHH-(01)->HHHHHHHHHHH
->>> minOperations(11)
-11
-
-# H-(11)->HH-(01)->HHH-(11)->HHHHHH-(01)->HHHHHHHHH
->>> minOperations(9)
-6
-
-# H-(11)->HH-(01)->HHH-(11)->HHHHHH-(11)->HHHHHHHHHHHH
->>> minOperations(12)
-7
-
-# H-(11)->HH-(01)->HHH-(11)->HHHHHH-(01)->HHHHHHHHH
->>> minOperations(15)
-8
+    # if got the desired result
+    if pasted_chars == n:
+        return counter
+    else:
+        return 0
